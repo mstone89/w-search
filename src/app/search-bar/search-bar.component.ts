2a222cc4c396e4ad77debbc32b6550a7bec4e672
Molly Stone <molly.stone89@gmail.com>
2020-02-17 07:51:04 -0500

    Communicate search term to parent app component

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
@@ -8,13 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class SearchBarComponent implements OnInit {
     term: string = '';
 
+    @Output() submitted = new EventEmitter<string>();
+
     constructor() { }
 
     ngOnInit() {
     }
 
-    onInput(value: string) {
-        this.term = value;
+    onFormSubmit(event: any) {
+        event.preventDefault();
+        this.submitted.emit(this.term);
+        console.log(this.term);
     }
 
 }
